1d817652d321d67885f96960f94771beed9f95f5
Hodaya Siman Tov <hodaysi123@gmail.com>
2021-07-23 02:23:42 +0300

    3

--- socialnetwork1.py
+++ socialnetwork1.py
@@ -47,34 +47,24 @@ import matplotlib.pyplot as plt
 
 
 #third try from csv and panda networkx  נסיון שלישי לפי
-df = pd.read_csv("musae_ENGB_edges.csv")                            #load data from csv file
-#df.head()
-snaG = nx.from_pandas_edgelist(df, source="from", target="to")
-#print(nx.info(snaG))                                               #graph information
-
+df = pd.read_csv("musae_ENGB_edges.csv") #load data from csv file
+# df = pd.read_csv("Test.csv")
+snaG = nx.from_pandas_edgelist(df, source="from", target="to")                                              #graph information
 max_grade = max(snaG.degree, key=lambda x: x[1])[1]
 Adam = [node[0] for node in snaG.degree if node[1] == max_grade]    #Node with the largest degree
 Adam = Adam[0]
 print("I am the node with the largest degree: ",Adam)
 
 
-nG = nx.Graph()                                                   #Create undirected graph for neighbors
-neighbors = list(snaG.neighbors(Adam))                            #List of Adam's neighbors
-
-for item in neighbors:
-    nG.add_node(item)                                             #Add node to neighbors graph
-    nG.add_edge(Adam,item)                                        #Add edge to neighbors graph
-nG = snaG.subgraph(neighbors)  
-pos = nx.spring_layout(nG)
-
-nx.draw(nG, pos, node_color = "lavender", 
-        node_size = 500, with_labels = True)  
+neighbors = list(snaG.neighbors(Adam)) #List of Adam's neighbors
+neighbors.append(Adam)
+#Create undirected graph for neighbors
+nG = snaG.subgraph(neighbors) 
+nx.draw_random(nG,node_color = "pink", with_labels = True)
 print("The Adam's neighbors graph  is beloe")
 plt.show()                                 #Add the edges between to Adam's neighbors
 # plt.savefig("AdamNeighborsGraph.png")
 
-# nx.draw_shell(nG,node_size = 500, with_labels = True)
-# plt.savefig("filename6.png")
 
 print(nx.info(nG))                                                #All the information of Adam's neighbor graph
 print( 'Number Connected Components: {0}'.format(nx.number_connected_components(nG)))
@@ -89,14 +79,48 @@ def plot_degree_dist(G):
     print("The degree distribution of Adam's neighbors graph  is beloe")
     plt.show()
 
-plot_degree_dist(nx.gnp_random_graph(100, 0.5, directed=True))
+plot_degree_dist(nG)
 
-ego_g = nx.ego_graph(nG, Adam)
-# d = dict(ego_g.degree)
-# nx.draw(ego_g, node_color='lightblue', 
-#         with_labels=True, 
-#         nodelist=d, 
-#         node_size=[d[k]*300 for k in d])
+print("The amount of triangles that includes Adam : {0}".format((nx.triangles(nG,(Adam,)))[Adam]))
+
+completeGraph=nx.complete_graph(len(nG.nodes), create_using=None)
+print("The  maximum number of triangles a network of Adam's network size is : {0}".format((nx.triangles(completeGraph,(1,)))[1]))
+
+neighbors.remove(Adam)
+neighborsG = nG.subgraph(neighbors) 
+max_grade = max(neighborsG.degree, key=lambda x: x[1])[1]
+BestfriendAdam = [node[0] for node in neighborsG.degree if node[1] == max_grade]    #Node with the largest degree
+BestfriendAdam = BestfriendAdam.pop(0)
+print("The best friend  of Adam is {0}".format(BestfriendAdam))
+
+bfG=nx.Graph()
+bfG.add_node(Adam)
+for i in neighbors:
+    if(neighborsG.has_edge(i,BestfriendAdam)):
+        bfG.add_node(i)
+        bfG.add_edge(i,BestfriendAdam)
+        bfG.add_edge(i,Adam)
+plt.clf()
+bfG.add_edge(Adam,BestfriendAdam)
+
+
+nx.draw_random(bfG,node_size =1200,node_color = "pink", with_labels = True)
+plt.show()
+
+
+print( 'Number Connected Components of subgroup of freing in Adam: {0}'.format(nx.number_connected_components(neighborsG)))
+plt.clf()
+nx.draw_random(neighborsG,node_size =1200,node_color = "pink", with_labels = True)
+plt.show()
+
+
+
+
+
+
+
+
+
 
 
 
