ea5af29f8343471db96cf4a41962a9939cd204a1
kineret levi <kineret56@gmail.com>
2021-07-22 15:37:40 +0300

    add

--- socialnetwork1.py
+++ socialnetwork1.py
@@ -1,17 +1,23 @@
 import snap
+import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 
-my_graph = nx.Graph()
-my_graph = nx.read_gml('celegansneural.gml')
+#first try networkx     נסיון ראשון לפי 
+# my_graph = nx.Graph()
+# my_graph = nx.read_gml('celegansneural.gml')
 
-print(nx.info(my_graph))
+# print(nx.info(my_graph))
 
-max_grade = max(my_graph.degree, key=lambda x: x[1])[1]
-Adam = [node[0] for node in my_graph.degree if node[1] == max_grade]  #Node with thw largest degree
-print(Adam)
+# max_grade = max(my_graph.degree, key=lambda x: x[1])[1]
+# Adam = [node[0] for node in my_graph.degree if node[1] == max_grade]  #Node with thw largest degree
+# print(Adam)
 
+# G = my_graph.neighbors(Adam)
+# G = nx.path_graph(4)  # or DiGraph, MultiGraph, MultiDiGraph, etc
+# G[0]
 
-#G = nx.path_graph(4)  # or DiGraph, MultiGraph, MultiDiGraph, etc
+# G = nx.path_graph(4)  # or DiGraph, MultiGraph, MultiDiGraph, etc
 # [n for n in my_graph.neighbors(Adam)]
 
 
@@ -19,15 +25,55 @@ print(Adam)
 # gml_graph = nx.read_gml("./celegansneural.gml")
 # nx.draw(gml_graph, with_labels=True, font_weight='bold')
 
-# G1 = snap.LoadEdgeList(snap.TUNGraph, "facebook_combined.txt", 0, 1)
 
-# Adam = G1.GetMxDegNId()    #Adam is the largest degree node
-# print(Adam)
+#second try snap   נסיון שני לפי
+# G1 = snap.LoadEdgeList(snap.TUNGraph, "facebook_combined.txt", 0, 1)   #Load the facebook combined graph using snap.py
 
-# neighbor_graph = G1.neighbors(Adam)    # Adam`s neighbors graph
+# Adam = G1.GetMxDegNId()                                                #Adam is the largest degree node
+# print("I am the node with the largest degree: ", Adam)
 
-# NodeNum, NodeVec = G1.GetNodesAtHop(Adam, 1, False)    #All the nodes that are in 0ne hop from Adam - neighberhood graph   
+# Gneighbors = snap.TUNGraph.New()                                       #Build Adam`s neighbors graph.     
+
+# edgeAmount = 0                                                         #Count the amount of edges in the neighbors graph
+# NodeNum, NodeVec = G1.GetNodesAtHop(Adam, 1, False)                    #All the nodes that are in one hop from Adam   
 # for item in NodeVec:
-#     print(item)
-#     print("num: " , NodeNum)
+#     Gneighbors.AddNode(item)                                           #Add node to neighbors graph
+#     Gneighbors.AddEdge(Adam,item)                                      #Add edge to neighbors graph
+#     edgeAmount += 1
+
+# print("Amount of nodes in adam's neighbors graph:  " , NodeNum)
+# print("Amount of edges in adam's neighbors graph:  " , edgeAmount)
+
+
+#third try from csv and panda networkx  נסיון שלישי לפי
+df = pd.read_csv("musae_ENGB_edges.csv")                            #load data from csv file
+#df.head()
+snaG = nx.from_pandas_edgelist(df, source="from", target="to")
+#print(nx.info(snaG))                                               #graph information
+
+max_grade = max(snaG.degree, key=lambda x: x[1])[1]
+Adam = [node[0] for node in snaG.degree if node[1] == max_grade]    #Node with the largest degree
+Adam = Adam[0]
+print("I am the node with the largest degree: ",Adam)
+
+
+nG = nx.Graph()                                                   #Create undirected graph for neighbors
+neighbors = list(snaG.neighbors(Adam))                            #List of Adam's neighbors
+
+for item in neighbors:
+    nG.add_node(item)                                             #Add node to neighbors graph
+    nG.add_edge(Adam,item)                                        #Add edge to neighbors graph
+
+nG = snaG.subgraph(neighbors)                                     #Add the edges between to Adam's neighbors
+print(nx.info(nG))                                                #All the information of Adam's neighbor graph
+
+
+
+
+
+
+
+
+
+
 
